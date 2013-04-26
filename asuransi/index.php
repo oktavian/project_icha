e7d38dab301158c85dd6e399b84a961d42d79ca2
oktavianus <oktavianus.benny@gmail.com>
2013-04-26 08:04:02 +0700

    upload application for insurance

---
+++ asuransi/index.php
@@ -0,0 +1,63 @@
+<?php
+    //$judul;
+?>
+<html>
+    <head>
+        <title>HOME</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script type="text/javascript" src="js/jquery-1.8.2.js"></script>
+    </head>
+    <body>
+        <div id="keterangan">
+           berisi logut, hak akses , anda berada di menu apa
+        </div>
+        <div id="navigasi">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">DSP</a></li>
+                <li><a href="#">NASABAH</a></li>
+                <li><a href="#">LAPORAN</a></li>
+            </ul>
+        </div>
+        <div id="outer">
+            <div id="header">
+                <img src="css/images/bringinglife.jpg" />
+                <h1>visi misi</h1>
+                <h1>visi misi</h1>
+                <h1>visi misi</h1>
+                <h1>visi misi</h1>
+                <h1>visi misi</h1>
+            </div>
+            <div id="content">
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+                <p>
+                    ISI DARI WEBSITE
+                </p>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </body>
+</html>
